56c86969bf63a67cc9314da13567e3ba5c9b9b2a
Rasmus_Dahlkvist <97rasdah@gmail.com>
2022-10-11 14:57:06 +0000

    Restarting gitpod

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,11 @@
-console.log("Hello World!");
+document.getElementById("rock-btn").onClick = function() {
+    console.log("Rock");
+}
+
+document.getElementById("paper-btn").onClick = function() {
+    console.log("Paper");
+}
+
+document.getElementById("scissors-btn").onClick = function() {
+    console.log("Scissors");
+}
