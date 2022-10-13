2a068b7ad01389b71bb4e91a1738cfb3ca7a2a10
Rasmus_Dahlkvist <97rasdah@gmail.com>
2022-10-13 16:19:22 +0000

    Add icons for game to be shown when button is pressed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,23 @@
+const rock = 0;
+const paper = 1;
+const scissors = 2;
+let playerIcons = document.getElementsByClassName("player-icons");
+
 document.getElementById("rock-btn").onclick = function() {
-    console.log("Rock");
+    
+    playerIcons[rock].style.display = "block";
+    playerIcons[paper].style.display = "none";
+    playerIcons[scissors].style.display = "none";
 }
 
 document.getElementById("paper-btn").onclick = function() {
-    console.log("Paper");
+    playerIcons[rock].style.display = "none";
+    playerIcons[paper].style.display = "block";
+    playerIcons[scissors].style.display = "none";
 }
 
 document.getElementById("scissors-btn").onclick = function() {
-    console.log("Scissors");
+    playerIcons[rock].style.display = "none";
+    playerIcons[paper].style.display = "none";
+    playerIcons[scissors].style.display = "block";
 }
