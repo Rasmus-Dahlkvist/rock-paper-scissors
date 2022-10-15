7f8754d600d8f4ec69c28c56409cee56cd859a8a
Rasmus_Dahlkvist <97rasdah@gmail.com>
2022-10-15 08:47:58 +0000

    Add faulty scores function in script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,20 @@
-const rock = 0;
-const paper = 1;
-const scissors = 2;
 let playerIcons = document.getElementsByClassName("player-icons");
 let computerIcons = document.getElementsByClassName("computer-icons");
 let computer;
+let player;
+let playerScore = 0;
+let computerScore = 0;
+const rock = 0;
+const paper = 1;
+const scissors = 2;
+
 
 document.getElementById("rock-btn").onclick = function() {
     playerIcons[rock].style.display = "block";
     playerIcons[paper].style.display = "none";
     playerIcons[scissors].style.display = "none";
     computerChoice();
+    scores();
 };
 
 document.getElementById("paper-btn").onclick = function() {
@@ -17,6 +22,7 @@ document.getElementById("paper-btn").onclick = function() {
     playerIcons[paper].style.display = "block";
     playerIcons[scissors].style.display = "none";
     computerChoice();
+    scores();
 };
 
 document.getElementById("scissors-btn").onclick = function() {
@@ -24,6 +30,7 @@ document.getElementById("scissors-btn").onclick = function() {
     playerIcons[paper].style.display = "none";
     playerIcons[scissors].style.display = "block";
     computerChoice();
+    scores();
 };
 
 function computerChoice (){
@@ -43,3 +50,36 @@ function computerChoice (){
         computerIcons[scissors].style.display = "block";
     }
 };
+
+function scores (){
+    let playerOutcome = "who wins?";
+    let computerOutcome = "who wins?";
+
+    if (computer == player){
+        computerOutcome = "Tie!";
+        playerOutcome = "Tie!"
+
+    } else if (
+        computer == rock && player == scissors ||
+        computer == paper && player == rock ||
+        computer == scissors && player == paper
+    ){
+        computerScore++;
+        computerOutcome = "Win!";
+        playerOutcome = "Lose!";
+
+    } else if (
+        player == rock && computer == scissors ||
+        player == paper && computer == rock ||
+        player == scissors && computer == paper
+    ){
+        playerScore++;
+        playerOutcome = "Win!";
+        computerOutcome = "Lose!";
+    }
+
+    document.getElementById("player-score").innerHTML = "Score:" + playerScore;
+    document.getElementById("player-outcome").innerHTML = playerOutcome;
+    document.getElementById("computer-score").innerHTML = "Score:" + computerScore;
+    document.getElementById("computer-outcome").innerHTML = computerOutcome;
+}
